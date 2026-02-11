0c634bd98b279b71a9b5db58f43fca3bc0cad5a7
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-11 18:49:13 -0300

    correction on 1 excercise in chapter 11 and added chapter 2 excercises

---
+++ python crash course book/chapter 2/excercises.py
@@ -0,0 +1,48 @@
+# 2.1
+message_one = "first message"
+print(f"{message_one}")
+
+# 2.2
+message_two = "second message"
+print(message_two)
+
+message_two = "Third message"
+print(message_two)
+
+
+# 2.3 and 2.4
+name = "steve"
+message = f"Hello {name} this is an automated message"
+
+print(name.lower())
+print(name.upper())
+print(name.capitalize())
+
+# 2.5 - 2.6
+print(f"\tAlbert Einstein once said 'a person who never makde a \n\tmistake never tried anything new'")
+
+famous_person = "Albert Einstein"
+quote = "a person who never makde a \n\tmistake never tried anything new"
+print(f"\t{famous_person} once said '{quote}'")
+
+# 2.7
+name_strip = "  Mario  "
+print(name_strip)
+print(name_strip.rstrip())
+print(name_strip.lstrip())
+print(name_strip.strip())
+
+
+# 2.8
+print(4+4)
+print(64/8)
+print(4*2)
+print(20-12)
+
+# 2.9
+fav_number = 7
+
+print(f"fav number is {fav_number}")
+
+
+# 2.10 this is a comment
