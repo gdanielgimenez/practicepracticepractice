f73ccb007c5b11e94674e351c9344b51cbd45570
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-12 23:25:01 -0300

    round 2 of conditionals excercises solved

---
+++ python crash course book/chapter 5/aliens.py
@@ -0,0 +1,73 @@
+# 5.3 alien colors 1
+alien_color = "green"
+if alien_color == "green":
+    print("+5 points player 1")
+alien_color = "yellow"
+if alien_color == "green":
+    print("+5 points player 1")
+
+# 5.4  alien colors 2
+alien_colors = "green"
+
+if alien_colors == "green":
+    print(f"+10 points player 1")
+else:
+    print("+5 points player 1")
+
+alien_colors_2 = "green"
+
+if alien_colors_2 == "green":
+    print(f"+10 points player 1")
+else:
+    print("+5 points player 1")
+
+# 5.5 alien colors 3
+aliens_colors = "green"
+if aliens_colors == "green":
+    print("+5 points awarded")
+elif aliens_colors == "yellow":
+    print("+10 points awarded")
+else:
+    print("+15 points awarded!")
+
+aliens_colors_1 = "yellow"
+if aliens_colors_1 == "green":
+    print("+5 points awarded")
+elif aliens_colors_1 == "yellow":
+    print("+10 points awarded")
+else:
+    print("+15 points awarded!")
+
+
+aliens_colors_2 = "red"
+if aliens_colors_2 == "green":
+    print("+5 points awarded")
+elif aliens_colors_2 == "yellow":
+    print("+10 points awarded")
+else:
+    print("+15 points awarded!")
+
+
+# 5-6 stages of life
+age = 66
+if age < 2:
+    print("you're a baby")
+elif age < 4:
+    print("you're a toddler")
+elif age < 13:
+    print("you're a kid")
+elif age < 20:
+    print("you're a teenager")
+elif age < 65:
+    print("you're an adult")
+else:
+    print("you're an elder person")
+# 5.7 favorite fruits
+favorite_fruits = ["bananas", "apple", "orange", "strawberry"]
+
+if "bananas" in favorite_fruits:
+    print("i love banana milkshake")
+if "apple" in favorite_fruits:
+    print("apples are very good for your heart")
+if "orange" in favorite_fruits:
+    print("i love orange juice")
