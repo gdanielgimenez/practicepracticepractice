f21208cf44dc6433c6c1b9c5a1bdebaaabfe771f
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-19 15:46:41 -0300

    own tic tac toe version now check for win victories, no longer give victories for 3 blank spaces and checks for draw after 9 turns all working

--- automate the boring stuff book/chapter 5/my_tic_tac.py
+++ automate the boring stuff book/chapter 5/my_tic_tac.py
@@ -18,30 +18,39 @@ def print_board(board):
 
 
 def victory_check(board):
-    if board['tl'] == board['tm'] and board['tm'] == board['tr'] and board['tl'] != ' ' or board['tm'] != ' ' or board['tr'] != ' ':
-        print(f"{turn} wins! top row victory")
-        return True
-    elif board['ml'] == board['mm'] and board['mm'] == board['mr'] and board['ml'] != ' ' or board['mm'] != ' ' or board['mr'] != ' ':
-        print(f"{turn} wins! mid row victory")
-        return True
-    elif board['ll'] == board['lm'] and board['lm'] == board['lr'] and board['ll'] != ' ' or board['lm'] != ' ' or board['lr'] != ' ':
-        print(f"{turn} wins! low row victory")
-        return True
-    elif board['tl'] == board['ml'] and board['ml'] == board['ll'] and board['tl'] != ' ' or board['ml'] != ' ' or board['ll'] != ' ':
-        print(f"{turn} wins! left column victory")
-        return True
-    elif board['tm'] == board['mm'] and board['mm'] == board['lm'] and board['tm'] != ' ' or board['mm'] != ' ' or board['lm'] != ' ':
-        print(f"{turn} wins! middle column victory")
-        return True
-    elif board['tr'] == board['mr'] and board['mr'] == board['lr'] and board['tr'] != ' ' or board['mr'] != ' ' or board['lr'] != ' ':
-        print(f"{turn} wins! right column victory")
-        return True
-    elif board['tl'] == board['mm'] and board['mm'] == board['lr'] and board['tl'] != ' ' or board['mm'] != ' ' or board['lr'] != ' ':
-        print(f"{turn} wins! diagonal left victory")
-        return True
-    elif board['tr'] == board['mm'] and board['mm'] == board['ll'] and board['tr'] != ' ' or board['mm'] != ' ' or board['ll'] != ' ':
-        print(f"{turn} wins! diagonal right victory")
-        return True
+
+    if board['tl'] == board['tm'] and board['tm'] == board['tr']:
+        if board['tl'] != ' ' or board['tm'] != ' ' or board['tr'] != ' ':
+            print(f"{turn} wins! top row victory")
+            return True
+    elif board['ml'] == board['mm'] and board['mm'] == board['mr']:
+        if board['ml'] != ' ' or board['mm'] != ' ' or board['mr'] != ' ':
+            print(f"{turn} wins! mid row victory")
+            return True
+    elif board['ll'] == board['lm'] and board['lm'] == board['lr']:
+        if board['ll'] != ' ' or board['lm'] != ' ' or board['lr'] != ' ':
+            print(f"{turn} wins! low row victory")
+            return True
+    elif board['tl'] == board['ml'] and board['ml'] == board['ll']:
+        if board['tl'] != ' ' or board['ml'] != ' ' or board['ll'] != ' ':
+            print(f"{turn} wins! left column victory")
+            return True
+    elif board['tm'] == board['mm'] and board['mm'] == board['lm']:
+        if board['tm'] != ' ' or board['mm'] != ' ' or board['lm'] != ' ':
+            print(f"{turn} wins! middle column victory")
+            return True
+    elif board['tr'] == board['mr'] and board['mr'] == board['lr']:
+        if board['tr'] != ' ' or board['mr'] != ' ' or board['lr'] != ' ':
+            print(f"{turn} wins! right column victory")
+            return True
+    elif board['tl'] == board['mm'] and board['mm'] == board['lr']:
+        if board['tl'] != ' ' or board['mm'] != ' ' or board['lr'] != ' ':
+            print(f"{turn} wins! diagonal left victory")
+            return True
+    elif board['tr'] == board['mm'] and board['mm'] == board['ll']:
+        if board['tr'] != ' ' or board['mm'] != ' ' or board['ll'] != ' ':
+            print(f"{turn} wins! diagonal right victory")
+            return True
     else:
         return False
 
@@ -49,18 +58,26 @@ def victory_check(board):
 # add a flag to quit the game
 game_over = False
 turn = "X"
+# turn counter to check when to start verifying victory and draw situations
+turn_number = 0
 while game_over != True:
     print_board(board)
     print('Turn for ' + turn + ' Move on which space? ')
     move = input()
     board[move] = turn
+    # check for victory:
+    turn_number += 1
+    if turn_number > 4 and turn_number < 9:
+        victory = victory_check(board)
+        if victory == True:
+            game_over = True
+    elif turn_number == 9:
+        print(f"is a draw nobody wins")
+        print(f"Game Over :(")
+        game_over = True
     if turn == 'X':
         turn = "O"
     else:
         turn = "X"
-     # check for victory:
-    victory = victory_check(board)
-    if victory == True:
-        game_over = True
 
 print_board(board)
