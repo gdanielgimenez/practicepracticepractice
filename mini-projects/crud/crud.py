a8e7d5b6df85108b49b1072532939a8b5971d3ef
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-20 16:53:45 -0300

    crud mini-program displaying the list refactored to be a function call for each mode reduced aprox 18 lines of code, all modes still working as intended

--- mini-projects/crud/crud.py
+++ mini-projects/crud/crud.py
@@ -1,15 +1,28 @@
 # user can create save delete or update to do list.
-# v1 start with only one list.
+# function do display current list
+def show_list(list):
+    print("Here's the current list : ")
+    print("------------------------------------------------------------ ")
+    i = 0
+    for key, value in to_do_dict.items():
+        i += 1
+        print(f"\t {i}. {key} : {value}")
+    print("------------------------------------------------------------ ")
+
+    #
+    # v1 start with only one list.
 to_do_list = []
 to_do_dict = {}
 # we transform out list into a dict where task are keys and values are completed or not
 active = True
 while active:
     choice = input(
-        "\nTo add items to the list type 'a' \nTo edit items type 'e' \nTo delete items type 'd' \nTo quit the program type 'q' \n: ")
+        "\nTo add items to the list type 'a' \nTo edit items type 'e' \nTo delete items type 'd' \nTo show list type 's' \nTo quit the program type 'q' \n: ")
     if choice.lower() == 'q':
         print("Goodbye!")
         active = False
+    elif choice.lower() == 's':
+        show_list(to_do_dict)
     elif choice == 'a':
         print("type down and then press enter to ad items to the list")
         item = input(" item : ")
@@ -18,22 +31,11 @@ while active:
         to_do_dict[item] = "Not completed"
         print("------------------item added to the list-------------------- ")
         # print the current list
-        print("Here's the current list : ")
-        i = 0
-        for key, value in to_do_dict.items():
-            i += 1
-            print(f"\t {i}. {key} : {value}")
-        print("------------------------------------------------------------ ")
+        show_list(to_do_dict)
     elif choice == 'e':
         edit_mode = input("type 'e' to edit mode or 's' for change status : ")
         # print the current list
-        print("Here's the current list : ")
-        print("-------------------------------------- ")
-        i = 0
-        for key, value in to_do_dict.items():
-            i += 1
-            print(f"\t {i}. {key} : {value}")
-        print("------------------------------------------------------------ ")
+        show_list(to_do_dict)
         if edit_mode == 'e':
             print("edit mode : type item number to edit ")
             task_number = int(input(" : "))
@@ -50,13 +52,8 @@ while active:
             to_do_dict[key_finder] = "Completed"
     elif choice == 'd':
         # print the current list
-        print("Here's the current list : ")
+        show_list(to_do_dict)
         print("---------------------delete mode---------------------------- ")
-        i = 0
-        for key, value in to_do_dict.items():
-            i += 1
-            print(f"\t {i}. {key} : {value}")
-        print("------------------------------------------------------------ ")
         print("delete mode : type item number to edit ")
         item_number = int(input(" : "))
         del to_do_dict[to_do_list[item_number-1]]
