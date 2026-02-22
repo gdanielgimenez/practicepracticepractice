1890080d5cdf11c22f2cc867b127ff71baf11c3a
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-22 02:02:19 -0300

    initial set up folders and basic screen for alien invasion game project

---
+++ python crash course book/part 2/alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """a class to store all the settings for our game"""
+
+    def __init__(self):
+        """initialize the game settings"""
+        # SCREEN settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (50, 50, 150)
