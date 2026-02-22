22155c157d98bf66db785c92f2b2040919c316a1
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-22 20:22:31 -0300

    ship now stays within the screen space, little refactor of key presses event functions and other small changes

--- python crash course book/part 2/alien_invasion/settings.py
+++ python crash course book/part 2/alien_invasion/settings.py
@@ -7,3 +7,5 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (250, 250, 250)
+        # ship settings
+        self.ship_speed_factor = 1.5
--- python crash course book/part 2/alien_invasion/ship.py
+++ python crash course book/part 2/alien_invasion/ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """initialize the ship and starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # load the ship image and get its rect.
         self.image = pygame.image.load('images/snoopy.bmp')
@@ -16,16 +17,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # store decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """update the ship's position based on movement flag"""
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        # update the ship's center value, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """draw the ship and its current location"""
