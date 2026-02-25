d25e282aab4ab475e69511010eed89c6871593bc
GustavoGimenez90 <gdanielgimenez@gmail.com>
2026-02-24 23:07:42 -0300

    learning logs home page and basic view +template, paths and files related added

---
+++ python crash course book/part 2/django/learning_log/learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Define urls patters for learning_logs  app"""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # home page
+    path('', views.index, name='index'),
+]
